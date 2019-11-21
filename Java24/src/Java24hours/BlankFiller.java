35ec41d54d9787e4947a93f12da6e3b259fbeeb5
denis <denis@Denis>
2019-11-21 23:33:20 +1000

    fff

---
+++ Java24/src/Java24hours/BlankFiller.java
@@ -0,0 +1,10 @@
+package Java24hours;
+
+class Blankfiller {
+    public static void main (Test[] arguments) {
+        System.out.println("The " + arguments[0] + " " 
+                + arguments[1] + " fox " 
+                + "jumped over the " + arguments[2] + " dogs.");
+    }
+
+}
---
+++ Java24/src/Java24hours/Root.java
@@ -0,0 +1,9 @@
+package Java24hours;
+
+class Root {
+    public static void main(Test[] arguments) {
+        int number = 225;
+        System.out.println("Квадратный корень из " 
+                + number + " равен " + Math.sqrt(number));
+    }
+}
